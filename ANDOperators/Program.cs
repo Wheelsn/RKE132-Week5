a7351f5ba61f32290955a78a0ce4041cfa8b7d60
Mart Kol <martin.kolesnik@tktk.ee>
2024-11-16 18:08:13 +0200

    Добавьте файлы проекта.

---
+++ ANDOperators/Program.cs
@@ -0,0 +1,21 @@
+﻿//Math >=90; Biology >=90; Chemistry >=90; 
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math score: ");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology score: ");
+biology = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry score: ");
+chemistry = Int32.Parse(Console.ReadLine());
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congrats you are accepted!");
+}
+else
+{
+    Console.WriteLine("You application cannot be approved");
+}
---
+++ OROperators/Program.cs
@@ -0,0 +1,25 @@
+﻿//Math >=90 OR Biology >=90 OR Chemistry >=90;
+
+// math && chemistry || math && biology || biology && chemistry
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter your Math score: ");
+math =Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Chemistry score: ");
+chemistry = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter your Biology score: ");
+biology = Int32.Parse(Console.ReadLine());
+
+if ((math>= 90  && chemistry >= 90) ||  (math>=90 && biology>=90) || (chemistry >= 90 && biology >= 90))
+{
+    Console.WriteLine("Congrats you are accepted!");
+}
+    
+else
+{
+    Console.WriteLine("You application cannot be approved");
+}
+  
